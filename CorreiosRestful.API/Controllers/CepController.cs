668743806e875f9196f15e65fe4d03e4f6a1c891
Emerson Soares <dev.emerson@gmail.com>
2012-10-12 00:06:13 -0400

    Created API project

---
+++ CorreiosRestful.API/Controllers/CepController.cs
@@ -0,0 +1,13 @@
+﻿using CorreiosRestful.API.ViewModels;
+using System.Web.Http;
+
+namespace CorreiosRestful.API.Controllers
+{
+    public class CepController : ApiController
+    {
+        public Endereco Get(string cep)
+        {
+            return new Endereco();
+        }
+    }
+}
---
+++ CorreiosRestful.API/ViewModels/Endereco.cs
@@ -0,0 +1,17 @@
+﻿namespace CorreiosRestful.API.ViewModels
+{
+    public class Endereco
+    {
+        public string Bairro { get; set; }
+
+        public string Cep { get; set; }
+
+        public string Cidade { get; set; }
+
+        public string Estado { get; set; }
+
+        public string Logradouro { get; set; }
+
+        public string Numeros { get; set; }
+    }
+}
