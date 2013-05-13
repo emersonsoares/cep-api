363c5fced7d42e089536aec917e02f47fb79c20f
Emerson Soares <dev.emerson@gmail.com>
2013-05-13 17:29:36 -0400

    Resolvido bug de caracteres quebrados

--- CorreiosRestful.API/Controllers/CepController.cs
+++ CorreiosRestful.API/Controllers/CepController.cs
@@ -9,19 +9,19 @@ namespace CorreiosRestful.API.Controllers
 {
     public class CepController : ApiController
     {
-        [OutputCacheWebApi(86400)]
-		public HttpResponseMessage Get(int cep)
+        //[OutputCacheWebApi(86400)]
+        public HttpResponseMessage Get(int cep)
         {
-			var requisicao = WebRequest.Create(ConfigurationManager.AppSettings["UriCorreios"]);
-			requisicao.ContentType = "application/x-www-form-urlencoded";
-			requisicao.Headers.Set(HttpRequestHeader.ContentEncoding, "utf-8");
-			requisicao.Method = "POST";
+            var requisicao = WebRequest.Create(ConfigurationManager.AppSettings["UriCorreios"]);
+            requisicao.ContentType = "application/x-www-form-urlencoded";
+            requisicao.Headers.Set(HttpRequestHeader.ContentEncoding, "iso-8859-1");
+            requisicao.Method = "POST";
 
-			var parser = new HTMLEnderecoParser(requisicao, cep);
+            var parser = new HTMLEnderecoParser(requisicao, cep);
 
-			if (parser.EhValido)
-				return Request.CreateResponse(HttpStatusCode.OK, parser.Endereco);
-			return Request.CreateErrorResponse(HttpStatusCode.NotFound, "CEP não encontrado!");
+            if (parser.EhValido)
+                return Request.CreateResponse(HttpStatusCode.OK, parser.Endereco);
+            return Request.CreateErrorResponse(HttpStatusCode.NotFound, "CEP não encontrado!");
         }
 
 
