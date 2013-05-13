57ff2ce736a8049c19a6c7b217c9eed1130e19cd
Emerson Soares <dev.emerson@gmail.com>
2013-05-13 16:26:22 -0400

    Retorno da api indentado

--- CorreiosRestful.API/App_Start/WebApiConfig.cs
+++ CorreiosRestful.API/App_Start/WebApiConfig.cs
@@ -13,12 +13,12 @@ namespace CorreiosRestful.API
                 name: "DefaultApi",
                 routeTemplate: "cep/{cep}",
                 defaults: new { controller = "Cep", action = "Get" },
-				constraints: new { cep = @"^\d+$" } // Apenas inteiros
+                constraints: new { cep = @"^\d+$" } // Apenas inteiros
             );
 
             GlobalConfiguration.Configuration.Formatters.Insert(0, new JsonpMediaTypeFormatter());
-            GlobalConfiguration.Configuration.Formatters.Remove(config.Formatters.XmlFormatter);
-            GlobalConfiguration.Configuration.Formatters.JsonFormatter.Indent = true;
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+            config.Formatters.JsonFormatter.Indent = true;
             config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
         }
     }
