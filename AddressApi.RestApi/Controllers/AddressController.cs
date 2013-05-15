4a2440c6c5bb7eb5b6d7df51b5d50613dfcb878b
Emerson Soares <dev.emerson@gmail.com>
2013-05-15 11:37:13 -0400

    Created address api

---
+++ AddressApi.RestApi/Controllers/AddressController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using AddressApi.Correios;
+
+namespace AddressApi.RestApi.Controllers
+{
+    public class AddressController : ApiController
+    {
+        public HttpResponseMessage Get(int zipCode)
+        {
+            var repository = new CorreiosRepository();
+
+            try
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, repository.GetAddress(zipCode));
+            }
+            catch (Exception)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Endereço não encontado!");
+            }
+        }
+    }
+}
---
+++ AddressApi.RestApi/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace AddressApi.RestApi.Controllers
+{
+    public class HomeController : Controller
+    {
+        [OutputCache(Duration = 86400)]
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Correios RESTful API";
+            return View();
+        }
+    }
+}
