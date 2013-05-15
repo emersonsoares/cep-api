7318bb52cf4661776712c57ee001bde6b9b8d256
Emerson Soares <dev.emerson@gmail.com>
2013-05-15 10:29:26 -0400

    Changed .gitignore

--- AddressApi.Correios/CorreiosMobileCrawler.cs
+++ AddressApi.Correios/CorreiosMobileCrawler.cs
@@ -1,12 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Configuration;
+using System.IO;
+using System.Net;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace AddressApi.Correios
 {
-    class CorreiosMobileCrawler
+    public class CorreiosMobileCrawler
     {
+        private readonly WebRequest _request;
+
+        public CorreiosMobileCrawler(int zipCode)
+        {
+            _request = WebRequest.Create(ConfigurationManager.AppSettings["UriCorreios"]);
+            _request.ContentType = "application/x-www-form-urlencoded";
+            _request.Headers.Set(HttpRequestHeader.ContentEncoding, "ISO-8859-1");
+            _request.Method = "POST";
+            var requestParams = Encoding.ASCII.GetBytes(string.Format("cepEntrada={0}&tipoCep=&cepTemp&metodo=buscarCep", zipCode));
+            _request.ContentLength = requestParams.Length;
+
+            var requestStream = _request.GetRequestStream();
+            requestStream.Write(requestParams, 0, requestParams.Length);
+            requestStream.Close();
+        }
+
+        private string GetPageContent()
+        {
+            var response = _request.GetResponse();
+
+            using (var responseStream = response.GetResponseStream())
+            {
+                if (responseStream != null)
+                    using (var reader = new StreamReader(responseStream, Encoding.GetEncoding("IS-8859-1")))
+                        return reader.ReadToEnd();
+                throw new EndOfStreamException();
+            }
+        }
+
+
     }
 }
